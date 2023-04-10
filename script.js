ba9c70513dca7a1a47da94578dbb73ce0b5e1893
hagi <gardeer1541@gmail.com>
2023-04-11 04:32:40 +0530

    Add an eventlistener to add-book button to pop up the form

--- script.js
+++ script.js
@@ -7,8 +7,27 @@ const library = (() => {
 })();
 
 const displayController = (() => {
+    const addBtn = document.querySelector(".add-book");
+    const deleteBtns = document.querySelectorAll(".delete");
 
+
+
+    addBtn.addEventListener("click", () => {
+        document.getElementById("myForm").style.display = "block";
+    });
+
+    deleteBtns.forEach(btn => {
+        btn.addEventListener("click", )
+    })
+
+
+
+    const test = () => {
+        console.log(deleteBtns)
+    }
+    return { test, openForm }
 })();
+
 
 const libraryController = (() => {
 
